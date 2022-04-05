bb6179b9902e5f3b0e44cbdcac1e0fe15bb0e5ac
ImranHossain5722 <imd572258@gmail.com>
2022-04-05 21:49:31 +0600

    working 404 page

--- src/Components/NoFound/NotFound.js
+++ src/Components/NoFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './NotFound.css'
 
 const NotFound = () => {
     return (
         <div>
-                   <h1>SorrY Not Found </h1> 
+                   <div className="col-12 col-md-6 col-lg-6 col-xl-6 m-2 dronImage">
+                       <h1 className='notFoungTitle'> Soory not Found </h1>
+                    <img src="images/img404.png" alt="" width={450} />
+                </div> 
 
         </div>
     );
